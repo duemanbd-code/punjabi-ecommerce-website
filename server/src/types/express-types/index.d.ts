6ee94dc83a7809647f890f54ed7e98884c14cc61
Miron Hasan <miron.hesan@gmail.com>
2025-12-31 19:37:58 +0600

    updated

--- server/src/types/express-types/index.d.ts
+++ server/src/types/express-types/index.d.ts
@@ -1,25 +1,25 @@
-// server/src/types/express-type/index.d.ts
+// // server/src/types/express-type/index.d.ts
 
-import * as express from 'express';
-import { User } from '../../models/user.model'; // Adjust path as needed
-import { JwtPayload } from 'jsonwebtoken';
+// import * as express from 'express';
+// import { User } from '../../models/user.model'; // Adjust path as needed
+// import { JwtPayload } from 'jsonwebtoken';
 
-declare global {
-  namespace Express {
-    interface Request {
-      admin?: any; // custom property for admin
-      user?: User | JwtPayload | any;
-      files?: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[] };
-    }
-  }
-}
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       admin?: any; // custom property for admin
+//       user?: User | JwtPayload | any;
+//       files?: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[] };
+//     }
+//   }
+// }
 
-// Export AuthRequest interface
-export interface AuthRequest extends express.Request {
-  user: User | JwtPayload | any;
-  admin?: any;
-  body: any;
-  params: any;
-  query: any;
-  files?: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[] };
-}
+// // Export AuthRequest interface
+// export interface AuthRequest extends express.Request {
+//   user: User | JwtPayload | any;
+//   admin?: any;
+//   body: any;
+//   params: any;
+//   query: any;
+//   files?: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[] };
+// }
--- server/src/types/express/express.d.ts
+++ server/src/types/express/express.d.ts
@@ -1,31 +1,16 @@
 // server/src/types/express/express.d.ts
 
-import { Request } from 'express';
-import { IAdmin } from '../models/admin.model';
+import 'express';
 
 declare global {
   namespace Express {
     interface Request {
-      admin?: IAdmin | any;
+      admin?: any;
       user?: any;
       file?: any;
       files?: any;
-      body: any;
-      params: any;
-      query: any;
-      headers: any;
     }
   }
 }
 
-// Export AuthRequest that includes everything
-export interface AuthRequest extends Request {
-  admin: any;
-  user?: any;
-  file?: any;
-  files?: any;
-  body: any;
-  params: any;
-  query: any;
-  headers: any;
-}
+// Remove the AuthRequest export - it's causing conflicts
