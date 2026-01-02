7e441c4bdeba2d4842395cc8f55a0298ad1126f7
Miron Hasan <miron.hesan@gmail.com>
2026-01-02 17:32:12 +0600

    U

--- client/next.config.js
+++ client/next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ['localhost', 'taskin-panjabi-server.onrender.com'],
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
