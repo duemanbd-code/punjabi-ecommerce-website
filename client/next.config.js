9e14b08826ab3083cc71ff1a8284b61f8fb9470c
abdul-halim-dev <jamesborn1518@gmail.com>
2026-01-06 23:40:45 +0600

    added many things

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false, // Change this to false
+    tsconfigPath: './tsconfig.json', // Explicitly point to tsconfig
   },
   eslint: {
     ignoreDuringBuilds: true,
@@ -13,4 +14,22 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: {
+//     domains: ['localhost', 'taskin-panjabi-server.onrender.com'],
+//     unoptimized: true,
+//   },
+// };
+
+// module.exports = nextConfig;
 
